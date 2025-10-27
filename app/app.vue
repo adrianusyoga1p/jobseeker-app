54046797d07afbf7026b4d5e2b85dcef7bf05dfb
adri <adri@tixfly.id>
2025-10-27 23:36:09 +0700

    feat: add wathc token and user

--- app/app.vue
+++ app/app.vue
@@ -1,11 +1,45 @@
 <script setup lang="ts">
+import type { ToasterProps } from "@nuxt/ui";
+
 useHead({
-  titleTemplate: (t?: string) => t ? `${t} - TixFly` : "TixFly",
+  titleTemplate: (t?: string) => t ? `${t} - Job Seeker` : "Job Seeker",
 });
+
+const toaster: ToasterProps = { position: "bottom-left" };
+const route = useRoute();
+const { token, user } = storeToRefs(useAuthStore());
+watch(
+  [token, user],
+  ([t, u]) => {
+    if (!t) {
+      if (!route.path.startsWith("/auth")) {
+        return navigateTo("/auth/sign-in");
+      }
+    }
+    else {
+      const defaultAppPath = u?.role === "admin" ? "/admin/job-list" : "/app/job-list";
+
+      if (route.path.startsWith("/auth")) {
+        return navigateTo(defaultAppPath);
+      }
+
+      if (u?.role !== "admin" && route.path.startsWith("/admin")) {
+        return navigateTo("/app/job-list");
+      }
+
+      if (u?.role === "admin" && route.path.startsWith("/app")) {
+        return navigateTo("/admin/job-list");
+      }
+    }
+  },
+  { immediate: true },
+);
 </script>
 
 <template>
-  <NuxtLayout>
-    <NuxtPage />
-  </NuxtLayout>
+  <UIApp :toaster="toaster">
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+  </UIApp>
 </template>
