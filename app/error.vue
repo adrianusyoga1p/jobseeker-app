e3be34be6eb167ea7cb8131af3ab243431dacf20
adri <adri@tixfly.id>
2025-10-27 23:36:27 +0700

    feat: add error state page

--- app/error.vue
+++ app/error.vue
@@ -1,3 +1,50 @@
+<script setup lang="ts">
+import type { NuxtError } from "#app";
+
+defineProps<{
+  error: NuxtError;
+}>();
+
+const { user, logout } = useAuthStore();
+</script>
+
 <template>
-  <p>Something went wrong</p>
+  <div class="w-screen h-screen max-w-md mx-auto flex items-center">
+    <UICard>
+      <h1 class="font-bold font-mono text-5xl text-center italic mb-2">
+        {{ error.statusCode }}
+      </h1>
+
+      <h1 class="font-mono text-2xl text-center mb-8">
+        {{ error.statusMessage }}
+      </h1>
+
+      <div class="flex items-center justify-center flex-col gap-2">
+        <NuxtLink to="/">
+          <base-button
+            size="lg"
+          >
+            Back to home
+          </base-button>
+        </NuxtLink>
+
+        <template v-if="user">
+          <p class="text-center my-4">
+            or
+          </p>
+
+          <base-button
+            size="lg"
+            color="outline"
+            @click="
+              logout();
+              navigateTo('/auth/sign-in');
+            "
+          >
+            Logout instead
+          </base-button>
+        </template>
+      </div>
+    </UICard>
+  </div>
 </template>
