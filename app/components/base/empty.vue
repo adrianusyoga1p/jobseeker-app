3354b4b98b03bc3a77f4da75866fd08adef1edaa
adri <adri@tixfly.id>
2025-10-28 12:53:36 +0700

    feat(comp): add empty component

---
+++ app/components/base/empty.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+defineProps<{
+  title?: string;
+  subtitle?: string;
+}>();
+</script>
+
+<template>
+  <div class="flex flex-col items-center justify-center gap-4 w-full py-20">
+    <img
+      src="/img/empty.png"
+      alt="empty"
+      class="w-[300px] h-[300px]"
+    >
+
+    <div class="space-y-1">
+      <h1 class="text-center text-netral-90 font-bold text-xl">
+        {{ title ?? 'No job openings available' }}
+      </h1>
+
+      <h3 class="text-center text-netral-90 font-medium">
+        {{ subtitle ?? 'Please wait for the next batch of openings.' }}
+      </h3>
+    </div>
+  </div>
+</template>
