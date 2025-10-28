fd0e26c91f9953365ae5daa3f8d471f12c9478ec
adri <adri@tixfly.id>
2025-10-28 23:57:27 +0700

    feat: adjust api add data

--- app/api/add-data.ts
+++ app/api/add-data.ts
@@ -2,6 +2,7 @@ import { addDoc, collection } from "firebase/firestore";
 
 export async function addData(dbName: string, data: any) {
   const { db } = await useFirebase();
+  const docRef = collection(db, dbName);
 
-  return await addDoc(collection(db, dbName), data);
+  return await addDoc(docRef, data);
 }
