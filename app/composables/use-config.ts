5903eb6e260e5fa0c951c0bc2cdaa25487397ffd
adri <adri@tixfly.id>
2025-10-29 06:33:15 +0700

    feat: adjust use-config

--- app/composables/use-config.ts
+++ app/composables/use-config.ts
@@ -1,18 +1,11 @@
 import production from "../config/env/production";
-import staging from "../config/env/staging";
 
 export default () => {
-  const { public: config } = useRuntimeConfig();
-
-  let envConfig: ENVConfig = staging;
-  if (config.env === "production")
-    envConfig = production;
+  const env = import.meta.env.MODE || "production";
 
   return {
-    env: config.env,
-    isProduction: config.env === "production",
-
-    // Environtment Config
-    ...envConfig,
+    env,
+    isProduction: true,
+    ...production,
   };
 };
