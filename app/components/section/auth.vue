5d00008e6ea75b8dce6729b222996832ff4ce685
adri <adri@tixfly.id>
2025-10-29 06:31:48 +0700

    feat: add section auth

---
+++ app/components/section/auth.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const { logo = false } = defineProps<{
+  logo?: boolean;
+}>();
+</script>
+
+<template>
+  <section class="w-full h-dvh flex items-center justify-center p-6">
+    <div class="space-y-6 max-w-md w-full">
+      <img
+        v-if="logo"
+        src="/img/logo.png"
+        class="h-12"
+        alt="logo"
+      >
+
+      <div class=" bg-white shadow p-10 space-y-4">
+        <slot />
+      </div>
+    </div>
+  </section>
+</template>
