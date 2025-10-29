10cc43c887780584e00942d85cfd297261d5ff2b
adri <adri@tixfly.id>
2025-10-29 07:31:10 +0700

    feat: add config prod

--- app/config/env/production.ts
+++ app/config/env/production.ts
@@ -1,3 +1,16 @@
 export default {
-  apiBaseUrl: "https://jsonplaceholder.typicode.com",
+  firebase: {
+    apiKey: "AIzaSyBbiH5yzbB1jaxY6tJK_QTv_bvqzv4THvY",
+    authDomain: "job-seeker-cee20.firebaseapp.com",
+    projectId: "job-seeker-cee20",
+    storageBucket: "job-seeker-cee20.firebasestorage.app",
+    messagingSenderId: "512737436031",
+    appId: "1:512737436031:web:ec6d2b507f276272c37a4a",
+    measurementId: "G-W834Z4CFEP",
+  },
+  cloudinary: {
+    cloudinaryName: "yoga-storage",
+    cloudinaryApiKey: "689525272229735",
+    cloudinaryApiSecret: "Z_8CCNcMBu0Hsa37JfzzK-smBL0",
+  },
 } satisfies ENVConfig;
